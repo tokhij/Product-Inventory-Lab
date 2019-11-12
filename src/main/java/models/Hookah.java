ec1936d60eb1773d6f5e363cf4bbf12e2d46f2f2
John Tokhi <56596400+tokhij@users.noreply.github.com>
2019-11-12 16:48:00 -0500

    added getters and setters

--- src/main/java/models/Hookah.java
+++ src/main/java/models/Hookah.java
@@ -1,8 +1,49 @@
 package models;
 
 public class Hookah {
+    //declare the variables
     private String brand;
     private Integer size;
     private int qty;
     private float price;
+
+
+    // set the getters and setters for the variables to be accessible
+    public String getBrand() {
+        return brand;
+    }
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+
+
+
+
+
+
 }
--- src/test/java/services/HookahServiceTest.java
+++ src/test/java/services/HookahServiceTest.java
@@ -1,4 +1,34 @@
 package services;
 
+import models.Hookah;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
 public class HookahServiceTest {
+
+    public class SneakerTest {
+
+        @Test
+        public void setBrandTest() {
+            // given (1)
+            String expected = "Khalil Mahmoon";
+
+            // when (2)
+            Hookah hookah1 = new Hookah();
+            hookah1.setBrand(expected);
+
+            // then (3)
+            Assertions.assertEquals(expected, hookah1.getBrand());
+        }
+    }
+
+
+
+
+
+
+
+
+
+
 }
